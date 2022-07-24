cb6186bde1e15dea13f8dfdbce0cbfcfe51c82eb
WorkingPranav <108082438+WorkingPranav@users.noreply.github.com>
2022-07-24 13:07:05 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,6 @@
 class script(object):
     START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, I CAN PROVIDE ALL COURCES FREE OF COST😍😍 I AND YOU CHANGE ONLINE COURSE FEE SUBSCRIBE @NOWREVOLUTION
+𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, I CAN PROVIDE ALL COURCES FREE OF COST😍😍 I AND YOU CHANGE ONLINE COURSE FEE SUBSCRIBE @NOWREVOLUTION"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
